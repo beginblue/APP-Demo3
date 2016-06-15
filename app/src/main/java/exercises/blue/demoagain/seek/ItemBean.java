729bbfe829b674d7a7e6f43f5c3191496ff8c208
blue <getbluer@gmail.com>
2016-06-16 00:12:34 +0800

    search

---
+++ app/src/main/java/exercises/blue/demoagain/seek/ItemBean.java
@@ -0,0 +1,16 @@
+package exercises.blue.demoagain.seek;
+
+/**
+ * Created by blue on 16-6-15.
+ */
+public class ItemBean {
+    public int ItemImageResid;
+    public String ItemTitle;
+    public String ItemContent;
+
+    public ItemBean(int itemImageResid, String itemTitle, String itemContent) {
+        ItemImageResid = itemImageResid;
+        ItemTitle = itemTitle;
+        ItemContent = itemContent;
+    }
+}
