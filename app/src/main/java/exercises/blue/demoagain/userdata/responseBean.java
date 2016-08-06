95b68f2a9d985b379863bc81ff6199c02dd9d9ec
beginblue <getbluer@gmail.com>
2016-08-06 11:26:45 +0800

    deleted some useless classes and renamed some new classes

--- app/src/main/java/exercises/blue/demoagain/userdata/responseBean.java
+++ app/src/main/java/exercises/blue/demoagain/userdata/responseBean.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Created by getbl on 2016/6/15.
  */
-public class beautyData {
+public class responseBean {
     /**
      * error : false
      * results : [{"_id":"5760b303421aa940e70aa911","createdAt":"2016-06-15T09:44:35.925Z","desc":"直接看图，，不用看描述。","publishedAt":"2016-06-15T11:55:46.992Z","source":"web","type":"福利","url":"http://ww1.sinaimg.cn/mw690/692a6bbcgw1f4fz6g6wppj20ms0xp13n.jpg","used":true,"who":"龙龙童鞋"},{"_id":"575e0824421aa9296bddf5a4","createdAt":"2016-06-13T09:11:00.530Z","desc":"直接看图，，不用看描述。","publishedAt":"2016-06-14T11:52:47.320Z","source":"web","type":"福利","url":"http://ww3.sinaimg.cn/mw690/81309c56jw1f4sx4ybttdj20ku0vd0ym.jpg","used":true,"who":"龙龙童鞋"},{"_id":"575cbba1421aa96b24382520","createdAt":"2016-06-12T09:32:17.746Z","desc":"跟上一个是一个系列的。","publishedAt":"2016-06-13T11:38:17.247Z","source":"web","type":"福利","url":"http://ww4.sinaimg.cn/mw690/9844520fjw1f4fqrpw1fvj21911wlb2b.jpg","used":true,"who":"龙龙童鞋"}]
